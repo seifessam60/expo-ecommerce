d378db4ecc4f530fe9287be344b5a51f7ab3c86a
Seif Essam <seifessam60@gmail.com>
2026-02-11 12:06:54 +0200

    added production config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,22 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import path from "path";
+import { ENV } from "../config/env.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = ENV.PORT;
+const __dirname = path.resolve();
 
 app.get("/api/health", (req, res) => {
   res.json({ message: "OK" });
 });
 
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
